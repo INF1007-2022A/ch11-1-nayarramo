21775b91a1b3739e64c4bc2d42e8ee90cbf52ccc
Omar-Rayan Belkhouala <112522847+nayarramo@users.noreply.github.com>
2022-12-10 21:54:38 -0500

    done

--- exercice.py
+++ exercice.py
@@ -2,7 +2,18 @@ from game import *
 
 
 def main():
-	pass
+	c1 = Character("Äpik", 200, 150, 70, 70)
+	c2 = Character("Gämmor", 250, 100, 120, 60)
+
+	c1.weapon = Weapon("BFG", 100, 69)
+	c2.weapon = Weapon("Deku Stick", 120, 1)
+
+	print(f"\n\nTest attack\n")
+	deal_damage(c1, c2)
+	print(f"\n------------\n")
+
+	turns = run_battle(c1, c2)
+	print(f"The battle ended in {turns} turns.")
 
 
 if __name__ == "__main__":
--- game.py
+++ game.py
@@ -3,11 +3,9 @@ Chapitre 11.1
 
 Classes pour représenter un personnage.
 """
+import random as rd
+from utils import clamp
 
-
-import random
-
-import utils
 
 
 class Weapon:
@@ -18,8 +16,22 @@ class Weapon:
 	:param power: Le niveau d'attaque
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
-
 	UNARMED_POWER = 20
+
+	def __init__(self, name, power, min_level):
+		self.__name = name
+		self.power = power
+		self.min_level = min_level
+
+	@property
+	def name(self):
+		return self.__name
+
+	@classmethod
+	def make_unarmed(clas):
+		return clas("Unarmed", clas.UNARMED_POWER, 1)
+
+
 
 
 class Character:
@@ -33,12 +45,77 @@ class Character:
 	:param level: Le niveau d'expérience du personnage
 	"""
 
+	def __init__(self, name, max_hp, attack, defense, level):
+		self.__name = name
+		self.max_hp = max_hp
+		self.attack = attack
+		self.defense = defense
+		self.level = level
+		self.weapon = None
+		self.hp = max_hp
 
-def deal_damage(attacker, defender):
+	@property
+	def name(self):
+		return self.__name
+
+	@property
+	def weapon(self):
+		return self.__weapon
+
+	@weapon.setter
+	def weapon(self, val):
+		if val == None: 
+			self.__weapon = Weapon.make_unarmed()
+		else: self.__weapon = val
+		
+	@property
+	def hp(self):
+		return self.__hp
+
+	@hp.setter
+	def hp(self, hp):
+		self.__hp = clamp(hp, 0, self.max_hp)
+
+
+
+	def compute_damage(self, defender: "Character"):
+		rand = rd.uniform(0.85, 1)
+		critical = rd.randint(1, 16) == 1
+		if critical: crit = 2
+		else: crit = 1
+		modifier = crit * rand
+		dmg =  (((((2* self.level / 5)+ 2) * self.weapon.power * (self.attack / defender.defense)) / 50) + 2) * modifier
+
+		return int(dmg), critical
+
+
+
+def deal_damage(attacker: "Character", defender: "Character"):
 	# TODO: Calculer dégâts
-	pass
+	dmg, crit = attacker.compute_damage(defender)
+	print(f"{attacker.name} used {attacker.weapon.name}\n  {defender.name} took {dmg} dmg")
+	return dmg
 
 
-def run_battle(c1, c2):
+def run_battle(c1: "Character", c2: "Character"):
 	# TODO: Initialiser attaquant/défendeur, tour, etc.
-	pass
+	print(f"{c1.name} ({c1.hp}) starts a battle with {c2.name} ({c2.hp})")
+	numberOfAttacks = 0
+	while c1.hp > 0 and c2.hp > 0:
+		if numberOfAttacks%2 == 0:
+			dmg = deal_damage(c1, c2)
+			c2.hp = c2.hp - dmg
+			print(f"{c2.name} now has {c2.hp} health points\n")
+		if numberOfAttacks%2 != 0:
+			dmg = deal_damage(c1, c2)
+			c1.hp = c1.hp - dmg
+			print(f"{c1.name} now has {c1.hp} health points\n")
+			
+		
+
+		numberOfAttacks += 1
+
+
+	if c1.hp == 0: print(f"{c1.name} is sleeping with the fishes")
+	if c2.hp == 0: print(f"{c2.name} is sleeping with the fishes")
+	return numberOfAttacks
